aee3aaaa4096026e1858ef5de356394a065a5b7b
AnatDean <anat.dean@hotmail.com>
2020-09-17 12:31:36 +0100

    add styling for quiz button type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,27 +8,45 @@ interface ButtonProps {
   onClick: () => void;
 }
 
+interface StyleProps {}
+
 export type ButtonComponentProps = {
   children: HTMLCollection | string;
   buttonType: string;
   onClick: (e?: React.MouseEvent) => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const progressButtonStyle = `
+const progressButtonStyle = (theme: any) => `
   text-align: center;
   width: 80%;
   height: ${rem('50px')};
+  background-color: ${theme.green};
+  color: ${theme.white};
 `;
+
+const quizButtonStyle = (theme: any): string => {
+  console.log(theme.pink);
+  return `
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  text-align: center;
+  width: 35vw;
+  height: ${rem('89px')};
+  background-color: ${theme.white}
+  color: ${theme.black};
+  &:focus {
+    color: ${theme.white};
+    background-color: ${theme.pink};
+  }
+`;
+};
 
 const StyledButton: React.FC<ButtonComponentProps> = styled.button<
   ButtonComponentProps
 >`
-  background-color: ${(props) =>
-    props.buttonType === 'primary' ? props.theme.green : props.theme.white};
-  color: ${(props) =>
-    props.buttonType === 'primary' ? props.theme.white : props.theme.black};
-
-  ${(props) => props.buttonType === 'primary' && progressButtonStyle}
+  ${(props) =>
+    props.buttonType === 'primary' && progressButtonStyle(props.theme)}
+  ${(props) => props.buttonType === 'quiz' && quizButtonStyle(props.theme)}
 `;
 
 const Button: React.FC<ButtonProps> = ({ children, buttonType, onClick }) => {
